500c0c5710d1e71f49d42c0fc73c72b349253e3e
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-06-28 13:04:27 +0200

    fixup

--- src/argus_htmx/templatetags/argus_htmx.py
+++ src/argus_htmx/templatetags/argus_htmx.py
@@ -27,6 +27,6 @@ def isackedby(incident, context=None) -> bool:
         if context.ack is None:
             is_acked = incident.acked
         else:
-            is_acked = incident.acked_by(context.ack)
+            is_acked = incident.is_acked_by(context.ack)
 
     return is_acked

ed01239e2ef0a39ac4b9fc8f4975fac723044bc4
Johanna England <johanna.england@sikt.no>
2024-09-12 12:11:06 +0200

    Add CSS linting using stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'at-rule-no-unknown': [
+            true,
+            {
+                ignoreAtRules: [
+                    'tailwind',
+                    'apply',
+                    'variants',
+                    'responsive',
+                    'screen',
+                ],
+            },
+        ],
+        'no-descending-specificity': null,
+    },
+}

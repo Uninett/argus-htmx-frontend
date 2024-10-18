149211ba842d1d5e90f9f205879643af62e77e21
Ilona Podliashanyk <ilona.podliashanyk@sikt.no>
2024-10-18 14:13:54 +0200

    Remove now obsolete ack incident view

--- src/argus_htmx/incidents/urls.py
+++ src/argus_htmx/incidents/urls.py
@@ -7,7 +7,6 @@ app_name = "htmx"
 urlpatterns = [
     path("", views.incident_list, name="incident-list"),
     path("<int:pk>/", views.incident_detail, name="incident-detail"),
-    path("<int:pk>/ack/", views.incident_add_ack, name="incident-add-ack"),
     path("update/<str:action>/", views.incidents_update, name="incidents-update"),
     path("<int:pk>/update/<str:action>/", views.incidents_update, name="incidents-update"),
 ]
--- src/argus_htmx/incidents/views.py
+++ src/argus_htmx/incidents/views.py
@@ -71,39 +71,6 @@ def incident_detail(request, pk: int):
     return render(request, "htmx/incidents/incident_detail.html", context=context)
 
 
-def _incident_add_ack(pk: int, formdata, user: User, group: Optional[str] = None):
-    incident = get_object_or_404(Incident, id=pk)
-    is_group_member = None
-    if group:
-        group = get_object_or_404(Group, name=group)
-        is_group_member = user.groups.filter(pk=group.pk).exists()
-    form = AckForm()
-    if formdata:
-        if group and not is_group_member:
-            raise PermissionDenied("User {request.user} is not a member of the correct group")
-        form = AckForm(formdata)
-        if form.is_valid():
-            incident.create_ack(
-                user,
-                description=form.cleaned_data["description"],
-                expiration=form.cleaned_data["expiration"],
-            )
-    context = {
-        "form": form,
-        "incident": incident,
-        "page_title": str(incident),
-        "group": group,
-        "is_group_member": is_group_member,
-    }
-    return incident, context
-
-
-def incident_add_ack(request, pk: int, group: Optional[str] = None):
-    formdata = request.POST or None
-    _, context = _incident_add_ack(pk, formdata, request.user, group)
-    return render(request, "htmx/incidents/incident_add_ack.html", context=context)
-
-
 def get_form_data(request, formclass: forms.Form):
     formdata = request.POST or None
     cleaned_form = None

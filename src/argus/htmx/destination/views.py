14797122583b40d2f1569d9a3c67169067d66ba4
Simon Oliver Tveit <simtve@uninett.no>
2024-10-30 16:30:12 +0100

    Add view for updating a destination

--- src/argus/htmx/destination/views.py
+++ src/argus/htmx/destination/views.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Sequence
 from django.shortcuts import render, get_object_or_404
 
 from django.views.decorators.http import require_http_methods
@@ -6,10 +6,11 @@ from django.http import HttpResponse
 
 from rest_framework.exceptions import ValidationError
 
+from argus.notificationprofile.models import DestinationConfig, Media
 from argus.notificationprofile.media import api_safely_get_medium_object
 from argus.notificationprofile.media.base import NotificationMedium
 
-from .forms import DestinationFormCreate
+from .forms import DestinationFormCreate, DestinationFormUpdate
 
 
 @require_http_methods(["GET", "POST"])
@@ -42,14 +43,64 @@ def destination_delete(request, pk: int) -> HttpResponse:
         return _render_destination_list(request)
 
 
-def _render_destination_list(request, create_form: Optional[DestinationFormCreate] = None) -> HttpResponse:
+@require_http_methods(["POST"])
+def destination_update(request, pk: int) -> HttpResponse:
+    destination = DestinationConfig.objects.get(pk=pk)
+    form = DestinationFormUpdate(request.POST or None, instance=destination, request=request)
+    if form.is_valid():
+        form.save()
+        return _render_destination_list(request)
+
+    update_forms = _get_update_forms(request.user)
+    for index, update_form in enumerate(update_forms):
+        if update_form.instance.pk == pk:
+            update_forms[index] = form
+            break
+    return _render_destination_list(request, update_forms=update_forms)
+
+
+def _render_destination_list(
+    request,
+    create_form: Optional[DestinationFormCreate] = None,
+    update_forms: Optional[Sequence[DestinationFormUpdate]] = None,
+) -> HttpResponse:
     """Function to render the destinations page.
 
     :param create_form: this is used to display the form for creating a new destination
-    with errors while retaining the user input. If you want a blank form, pass None."""
+    with errors while retaining the user input. If you want a blank form, pass None.
+    :param update_forms: list of update forms to display. Useful for rendering forms
+    with error messages while retaining the user input.
+    If this is None, the update forms will be generated from the user's destinations."""
+
     if create_form is None:
         create_form = DestinationFormCreate()
+    if update_forms is None:
+        update_forms = _get_update_forms(request.user)
+    grouped_forms = _group_update_forms_by_media(update_forms)
     context = {
         "create_form": create_form,
+        "grouped_forms": grouped_forms,
     }
     return render(request, "htmx/destinations/destination_list.html", context=context)
+
+
+def _get_update_forms(user) -> list[DestinationFormUpdate]:
+    # Sort by oldest first
+    destinations = user.destinations.all().order_by("pk")
+    return [DestinationFormUpdate(instance=destination) for destination in destinations]
+
+
+def _group_update_forms_by_media(
+    destination_forms: Sequence[DestinationFormUpdate],
+) -> dict[Media, list[DestinationFormUpdate]]:
+    grouped_destinations = {}
+
+    # Adding a media to the dict even if there are no destinations for it
+    # is useful so that the template can render a section for that media
+    for media in Media.objects.all():
+        grouped_destinations[media] = []
+
+    for form in destination_forms:
+        grouped_destinations[form.instance.media].append(form)
+
+    return grouped_destinations

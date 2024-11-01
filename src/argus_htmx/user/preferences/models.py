06de5f43574bb2c826cbab2e68731cab341dc425
Hanne Moa <hanne.moa@sikt.no>
2024-11-01 12:07:22 +0100

    update from argus userprefs branch

--- src/argus_htmx/user/preferences/models.py
+++ src/argus_htmx/user/preferences/models.py
@@ -1,32 +1,19 @@
-from django.core.exceptions import ValidationError
-
 from argus.auth.models import Preferences, PreferencesManager
 
 
 class ArgusHtmxPreferencesManager(PreferencesManager):
     def get_queryset(self):
-        return super().get_queryset().filter(namespace=NAMESPACE)
+        return super().get_queryset().filter(namespace=ArgusHtmxPreferences._namespace)
 
     def create(self, **kwargs):
-        kwargs["namespace"] = NAMESPACE
+        kwargs["namespace"] = ArgusHtmxPreferences._namespace
         return super().create(**kwargs)
 
 
 class ArgusHtmxPreferences(Preferences):
+    _namespace = "argus_htmx"
+
     class Meta:
         proxy = True
 
-    app_label = "argus_htmx"
-    class_name = "ArgusHtmxPreferences"
-
     objects = ArgusHtmxPreferencesManager()
-
-    # Try writing a validator using the form instead of changing clean_fields
-
-    def clean(self):
-        form = self.FORM(self.preferences)
-        if not form.is_valid():
-            raise ValidationError(form.errors)
-
-
-NAMESPACE = ArgusHtmxPreferences.generate_namespace()

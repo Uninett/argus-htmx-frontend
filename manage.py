9930795bb8712adc29fe0a46fb730861b525699c
Hanne Moa <hanne.moa@sikt.no>
2024-11-08 13:02:21 +0100

    lint and reformat

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python3
 
-import sys
 import django
 
 from django.conf import settings
@@ -9,15 +8,15 @@ from django.core.management import call_command
 settings.configure(
     DEBUG=True,
     INSTALLED_APPS=(
-        'django.contrib.contenttypes',
-        'django.contrib.auth',
-        'argus.auth',
-        'argus.incident',
-        'argus.notificationprofile',
-        'argus_htmx',
+        "django.contrib.contenttypes",
+        "django.contrib.auth",
+        "argus.auth",
+        "argus.incident",
+        "argus.notificationprofile",
+        "argus_htmx",
     ),
     MEDIA_PLUGINS=(),
 )
 
 django.setup()
-call_command('makemigrations', 'argus_htmx')
+call_command("makemigrations", "argus_htmx")

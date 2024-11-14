992735a20b8af5ee171ceaa342ee5ce6029520ad
Pelle Koster <pelle.koster@geant.org>
2024-11-14 13:58:47 +0100

    Use django settings for stylesheet path contextprocessor

--- src/argus_htmx/context_processors.py
+++ src/argus_htmx/context_processors.py
@@ -7,8 +7,9 @@ Append the "context_processors" list for the TEMPLATES-backend
 See django settings for ``TEMPLATES``.
 """
 
-from .settings import STYLESHEET_PATH
+from . import settings as fallback
+from django.conf import settings
 
 
 def path_to_stylesheet(request):
-    return {"path_to_stylesheet": STYLESHEET_PATH}
+    return {"path_to_stylesheet": getattr(settings, "STYLESHEET_PATH", fallback.STYLESHEET_PATH)}

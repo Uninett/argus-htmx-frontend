095ffa16f765c3cdbeea0e2fa5a9452e746f4170
Hanne Moa <hanne.moa@sikt.no>
2024-11-14 08:26:09 +0100

    fix check

--- src/argus_htmx/checks.py
+++ src/argus_htmx/checks.py
@@ -1,4 +1,4 @@
-from django.core.checks import Error, register
+from django.core.checks import Error, Warning, register
 from django.core.exceptions import ImproperlyConfigured
 
 from .themes.utils import get_theme_names
@@ -6,25 +6,22 @@ from .themes.utils import get_theme_names
 
 @register
 def check_for_valid_themes_list(app_configs, **kwargs):
-    errors = []
     themes = []
     try:
         themes = get_theme_names(quiet=False)
     except ImproperlyConfigured as e:
-        errors.append(
-            Error(
+        return [
+            Warning(
                 str(e),
                 hint="Regenerate styles.css",
                 id="argus_htmx.T001",
             )
-        )
-    else:
-        if not themes:
-            errors.append(
-                Error(
-                    "no themes installed",
-                    hint='Check the settings "DAISYUI_THEMES" and "TAILWIND_THEME_OVERRIDE" and regenerate styles.css',
-                    id="argus_htmx.T002",
-                )
+        ]
+    if not themes:
+        return [
+            Error(
+                "no themes installed",
+                hint='Check the settings "DAISYUI_THEMES" and "TAILWIND_THEME_OVERRIDE" and regenerate styles.css',
+                id="argus_htmx.T002",
             )
-    return errors
+        ]
